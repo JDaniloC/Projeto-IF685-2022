502ed864e9176ff476755091e832f62b45404810
JDaniloC <jdsc@cin.ufpe.br>
2022-10-05 00:41:43 -0300

    Nested table and clean code

--- alugueis/SQL/objeto_relacional/create_table_or.sql
+++ alugueis/SQL/objeto_relacional/create_table_or.sql
@@ -1,3 +1,5 @@
+-- Criação dos tipos
+
 CREATE OR REPLACE TYPE tp_endereco AS OBJECT (
     cep VARCHAR(50),
     complemento VARCHAR2(50),
@@ -8,6 +10,7 @@ CREATE OR REPLACE TYPE tp_endereco AS OBJECT (
     FINAL MAP MEMBER FUNCTION get_cep RETURN VARCHAR
 );
 /
+
 ALTER TYPE tp_endereco
 ADD ATTRIBUTE numero NUMBER CASCADE;
 /
@@ -26,7 +29,7 @@ END;
 /
 
 CREATE OR REPLACE TYPE tp_telefone AS OBJECT (
-    telefone VARCHAR2(11) -- Exemplo: 81988556633
+    numero VARCHAR2(11) -- Exemplo: 81988556633
 );
 /
 
@@ -38,30 +41,46 @@ CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
     nome VARCHAR2(40),
     sexo VARCHAR2(1),
     data_nasc DATE,
-    telefone tp_array_telefone,
+    telefones tp_array_telefone,
     endereco REF tp_endereco,
-    MEMBER PROCEDURE imprimir_informacoes
+    MEMBER PROCEDURE imprimir_informacoes,
+    ORDER MEMBER FUNCTION compara_idade (
+        SELF IN OUT NOCOPY tp_pessoa, e tp_pessoa
+    ) RETURN NUMBER
 ) NOT FINAL NOT INSTANTIABLE;
 /
 
 CREATE OR REPLACE TYPE BODY tp_pessoa AS
     MEMBER PROCEDURE imprimir_informacoes(SELF IN OUT NOCOPY tp_pessoa) IS
         BEGIN
-            dbms_output.put_line('CPF: ' || cpf);
-            dbms_output.put_line('Nome: ' || nome);
-            dbms_output.put_line('Sexo: ' || sexo);
-            dbms_output.put_line('Data nascimento: ' || data_nasc);
-            dbms_output.put_line('Telefone primário: ' || telefone(0));
+            DBMS_OUTPUT.PUT_LINE('CPF: ' || cpf);
+            DBMS_OUTPUT.PUT_LINE('Nome: ' || nome);
+            DBMS_OUTPUT.PUT_LINE('Sexo: ' || sexo);
+            DBMS_OUTPUT.PUT_LINE('Data nascimento: ' || data_nasc);
+            DBMS_OUTPUT.PUT_LINE('Telefone primário: ' || telefone(0));
+        END;
+    
+    ORDER MEMBER FUNCTION compara_idade (
+        SELF IN OUT NOCOPY tp_pessoa, e tp_pessoa
+    ) RETURN NUMBER IS
+        BEGIN
+            IF SELF.idade < e.idade THEN 
+                RETURN -1;
+            ELSIF SELF.idade > e.idade THEN 
+                RETURN 1;
+            ELSE 
+                RETURN 0;
+            END IF;
         END;
 END;
 /
+
 CREATE OR REPLACE TYPE tp_corretor UNDER tp_pessoa (
     data_admissao DATE,
     salario NUMBER,
-    cpf_supervisor REF tp_corretor,
+    supervisor REF tp_corretor,
     CONSTRUCTOR FUNCTION tp_corretor (corretor tp_corretor) RETURN SELF AS RESULT,
-    MEMBER FUNCTION get_salario RETURN NUMBER,
-    ORDER MEMBER FUNCTION compara_salario RETURN NUMBER,
+    MEMBER FUNCTION get_salario RETURN NUMBER, 
     OVERRIDING MEMBER PROCEDURE imprimir_informacoes
 );
 /
@@ -75,71 +94,37 @@ CREATE OR REPLACE TYPE BODY tp_corretor AS
             data_nasc := corretor.data_nasc;
             telefone := corretor.telefone;
             endereco := corretor.endereco;
+            data_admissao := corretor.data_admissao;
+            salario := corretor.salario;
+            supervisor := corretor.supervisor;
             RETURN;
         END;
-    ORDER MEMBER FUNCTION compara_salario (SELF IN OUT NOCOPY tp_endereco, e tp_endereco) RETURN NUMBER IS
-        BEGIN
-            IF SELF.salario < e.salario THEN 
-                RETURN -1;
-            ELSIF SELF.salario > e.salario THEN 
-                RETURN 1;
-            ELSE 
-                RETURN 0;
-            END IF;
-        END;
+
     MEMBER FUNCTION get_salario RETURN NUMBER IS
         BEGIN
             RETURN salario
         END;
+
     OVERRIDING MEMBER PROCEDURE imprimir_informacoes IS
         BEGIN
             dbms_output.put_line('CPF: ' || cpf);
             dbms_output.put_line('Nome: ' || nome);
             dbms_output.put_line('Sexo: ' || sexo);
+            dbms_output.put_line('salario: ' || salario);
             dbms_output.put_line('Data nascimento: ' || data_nasc);
+            dbms_output.put_line('Data admissão: ' || data_admissao);
             dbms_output.put_line('Telefone primário: ' || telefone(0));
-            dbms_output.put_line('CPF: ' || cpf);
-            dbms_output.put_line('Nome: ' || nome);
-            dbms_output.put_line('Sexo: ' || sexo);
+            dbms_output.put_line('CPF do supervisor ' || DEREF(supervisor).cpf);
 END;
 /
 
-CREATE OR REPLACE TYPE tp_cliente UNDER tp_pessoa(
-    cpf_cliente VARCHAR(14)
-);
+CREATE OR REPLACE TYPE tp_cliente UNDER tp_pessoa() NOT FINAL NOT INSTANTIABLE;
 /
-
-CREATE OR REPLACE TYPE tp_locador UNDER tp_pessoa(
-    cpf_locador VARCHAR(14)
-);
+CREATE OR REPLACE TYPE tp_locador UNDER tp_cliente();
 /
-
-CREATE OR REPLACE TYPE tp_locatario UNDER tp_pessoa(
-    cpf_locatario VARCHAR(14)
-);
+CREATE OR REPLACE TYPE tp_locatario UNDER tp_cliente();
 /
-
-CREATE OR REPLACE TYPE tp_fiador UNDER tp_pessoa(
-    cpf_fiador VARCHAR(14)
-);
-/
-
-CREATE OR REPLACE TYPE tp_sala AS OBJECT(
-    codigo INTEGER,
-    area NUMBER(6, 2),
-    endereco_sala REF tp_endereco,
-    cpf_adm REF tp_corretor,
-    cpf_dono REF tp_locador
-);
-/
-
-
-CREATE OR REPLACE TYPE tp_avaliacao AS OBJECT(
-	codigo_sala INTEGER,
-	preco_unit NUMBER(6, 2),
-    cpf_corretor REF tp_corretor,
-    cpf_locador REF tp_locador
-);
+CREATE OR REPLACE TYPE tp_fiador UNDER tp_cliente();
 /
 
 CREATE OR REPLACE TYPE tp_lucro AS OBJECT(
@@ -151,103 +136,60 @@ CREATE OR REPLACE TYPE tp_lucro AS OBJECT(
 CREATE OR REPLACE TYPE tp_aluguel AS OBJECT(
   	data_inicio DATE,
   	data_final DATE,
-    cpf_dono REF tp_locador,
-    cpf_fiador REF tp_fiador,
-    cpf_locatario REF tp_locatario,
-  	porc_locador REF tp_lucro,
-    codigo_sala REF tp_sala,
+  	luro tp_lucro,
+    fiador tp_fiador,
+    locatario tp_locatario
 );
 /
 
--- 1. CREATE OR REPLACE TYPE ✅
--- 2. CREATE OR REPLACE TYPE BODY ✅
--- 3. MEMBER PROCEDURE -> Joao
--- 4. MEMBER FUNCTION ✅
--- 5. ORDER MEMBER FUNCTION ✅
--- 6. MAP MEMBER FUNCTION ✅
--- 7. CONSTRUCTOR FUNCTION -> Joao
--- 8. OVERRIDING MEMBER -> Joao
--- 9. FINAL MEMBER ✅
--- 10. NOT INSTANTIABLE TYPE/MEMBER ✅
--- 11. HERANÇA DE TIPOS (UNDER/NOT FINAL) ✅
--- 12. ALTER TYPE ✅
--- 13. CREATE TABLE OF ✅
--- 14. WITH ROWID REFERENCES ✅
--- 15. REF ✅
--- 16. SCOPE IS ✅
--- 17. INSERT INTO
--- 18. VALUE
--- 19. VARRAY ✅
--- 20. NESTED TABLE
+CREATE OR REPLACE TYPE tp_nt_aluguel AS TABLE OF tp_aluguel;
+/
+
+CREATE OR REPLACE TYPE tp_sala AS OBJECT(
+    codigo INTEGER,
+    dono tp_locador,
+    area NUMBER(6, 2),
+    alugueis tp_nt_aluguel,
+	preco_unit NUMBER(6, 2),
+    endereco REF tp_endereco,
+    administrador tp_corretor
+);
+/
+
+-- Criação das tabelas
 
 CREATE TABLE tb_endereco OF tp_endereco (
     CONSTRAINT cep_pkey PRIMARY KEY(cep)
 );
 /
 
-CREATE TABLE tb_pessoa OF tp_pessoa (
+CREATE TABLE tb_corretor OF tp_corretor (
     CONSTRAINT cpf_pkey PRIMARY KEY(cpf),
     CONSTRAINT sexo_check CHECK (sexo IN ('M', 'F')),
-    endereco WITH ROWID REFERENCES tb_endereco
-);
-/
-
-CREATE TABLE tb_telefone OF tp_telefone (
-    CONSTRAINT telefone_pkey PRIMARY KEY(cpf_dono, codigo)
-);
-/
-
-CREATE TABLE tb_corretor OF tp_corretor (
-    CONSTRAINT cpf_corretor_pkey PRIMARY KEY(cpf),
-    cpf_supervisor SCOPE IS tb_corretor
-);
-/
-
-CREATE TABLE tb_cliente OF tp_cliente (
-    CONSTRAINT cpf_cliente_pkey PRIMARY KEY(cpf)
-);
-/
-
-CREATE TABLE tb_locador OF tp_locador (
-    CONSTRAINT cpf_locador_pkey PRIMARY KEY(cpf)
-);
-/
-
-CREATE TABLE tb_locatario OF tp_locatario (
-    CONSTRAINT cpf_locatario_pkey PRIMARY KEY(cpf)
-);
-/
-
-CREATE TABLE tb_fiador OF tp_fiador (
-    CONSTRAINT cpf_fiador_pkey PRIMARY KEY(cpf)
+    endereco WITH ROWID REFERENCES tb_endereco,
+    supervisor SCOPE IS tb_corretor
 );
 /
 
 CREATE TABLE tb_sala OF tp_sala (
-    CONSTRAINT sala_pkey PRIMARY KEY(codigo, cpf_dono),
-    endereco WITH ROWID REFERENCES tb_endereco,
-    cpf_dono WITH ROWID REFERENCES tb_locador,
-    cpf_adm WITH ROWID REFERENCES tb_corretor
-);
+    CONSTRAINT sala_pkey PRIMARY KEY(codigo),
+    endereco WITH ROWID REFERENCES tb_endereco
+) NESTED TABLE alugueis STORE AS tb_alugueis;
 /
 
-CREATE TABLE tb_avaliacao OF tp_avaliacao (
-	CONSTRAINT avaliacao_pkey PRIMARY KEY(codigo_sala, cpf_locador, cpf_corretor),
-    cpf_corretor WITH ROWID REFERENCES tb_corretor,
-    cpf_locador WITH ROWID REFERENCES tb_locador
+-- Inserções e consultas
+INSERT INTO tb_endereco VALUES (tp_endereco(
+    '55123456', 'A', 'Bairro Leal', 'Rua 9',
+    'Vitória de todos os santos', 'PE'
+));
+INSERT INTO tb_corretor VALUES (
+    '12345678910', 'George Matias', 'M',
+    TO_DATE('10/02/2000', 'dd/mm/yyyy'),
+    tp_array_telefone(
+        tp_telefone('81912345678'),
+        tp_telefone('81998765432')
+    ),
+    (SELECT REF(t) FROM tb_endereco t WHERE cep = '55123456'),
+    TO_DATE('10/02/2000', 'dd/mm/yyyy'), 0, NULL
 );
-/
-
-CREATE TABLE tb_lucro OF tp_lucro ( 
-	CONSTRAINT porc_locador_pkey PRIMARY KEY(porc_locador) 
-);
-/
-
-CREATE TABLE tb_aluguel OF tp_aluguel (
-	CONSTRAINT alguel_pkey PRIMARY KEY(codigo_sala, cpf_locatario, cpf_dono, data_inicio),
-    cpf_dono WITH ROWID REFERENCES tb_locador
-    cpf_fiador WITH ROWID REFERENCES tb_fiador,
-    cpf_locatario WITH ROWID REFERENCES tb_locatario,
-    proc_locador WITH ROWID REFERENCES tb_lucro,
-  	codigo_sala WITH ROWID REFERENCES tb_sala
-);
+SELECT VALUE(t).cpf AS cpf_corretor FROM tb_corretor t;
